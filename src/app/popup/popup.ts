27238890380461059da150592b88c93b2ce38703
ShaneDsouza4 <shane.json96@gmail.com>
2025-07-05 00:40:39 -0400

    Installed tailwind and resolved inline styling issues in Chrome build

--- src/app/popup/popup.ts
+++ src/app/popup/popup.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-popup',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './popup.html',
   styleUrl: './popup.css'
 })
-export class Popup {
+export class Popup implements OnInit {
+
+  today = new Date().toISOString().split("T")[0];
+  summary: Record<string, number> = {};
+  details: { domain: string, time: number, category: string }[] = [];
+
+  ngOnInit() {
+    const key = `fokys_${this.today}`;
+
+    chrome.storage.local.get([key], (result) => {
+      const data = result[key] || { summary: {}, details: [] };
+      this.summary = data.summary;
+      this.details = data.details;
+    });
+  }
+
+  formatTime(seconds: number): string {
+    const mins = Math.floor(seconds / 60);
+    const hrs = Math.floor(mins / 60);
+    const remMins = mins % 60;
+    return hrs > 0 ? `${hrs}h ${remMins}m` : `${mins}m`;
+  }
 
 }
